886892154bb4cde75acb788d267658267a92ea10
echen94 <chenyiying@ucla.edu>
2015-04-02 21:53:15 -0700

    testing git

--- alloc.h
+++ alloc.h
@@ -2,4 +2,6 @@
 #include <stddef.h>
 void *checked_malloc (size_t);
 void *checked_realloc (void *, size_t);
-void *checked_grow_alloc (void *, size_t *);
+void *checked_grow_alloc (void *, size_t *) ;
+
+
